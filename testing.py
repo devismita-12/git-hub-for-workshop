f4821e013f8f68f4ae173bb7d23a0951573972f0
Ubuntu <ubuntu@ip-172-31-80-128.ec2.internal>
2024-10-26 09:07:57 +0000

    add test file

---
+++ testing.py
@@ -0,0 +1,5 @@
+2. Security
+The Linux security feature is the main reason that it is the most favorable option for developers. It is not completely safe, but it is less vulnerable than others. Each application needs to authorize by the admin user. The virus is not executed until the administrator provides the access password. Linux systems do not require any antivirus program.
+
+3. Free
+Certainly, the biggest advantage of the Linux system is that it is free to use. We can easily download it, and there is no need to buy the license for it. It is distributed under GNU GPL (General Public License). Comparatively, we have to pay a huge amount for the license of the other operating systems.
